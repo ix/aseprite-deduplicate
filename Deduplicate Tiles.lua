49259aaa9d5b8eea8629ac26b67aa650a1479a91
Rose <rose@empty.town>
2021-01-09 10:36:58 +0000

    initial version

---
+++ Deduplicate Tiles.lua
@@ -0,0 +1,69 @@
+if app.activeSprite then
+  local sprite = app.activeSprite
+  local image = app.activeImage
+  local sizeX, sizeY = sprite.gridBounds.width, sprite.gridBounds.height
+  local tilesX, tilesY = sprite.width / sizeX, sprite.height / sizeY
+
+  function tileEquals(x1, y1, x2, y2)
+    for y = 0, sizeY - 1 do
+      for x = 0, sizeX - 1 do
+        if image:getPixel(x1 * sizeX + x, y1 * sizeY + y) ~= image:getPixel(x2 * sizeX + x, y2 * sizeY + y) then
+          return false
+        end
+      end
+    end
+
+    return true
+  end
+
+  function getEmptyColor()
+    if sprite.colorMode == ColorMode.RGB then
+      return app.pixelColor.rgba(0, 0, 0, 0)
+    elseif sprite.colorMode == ColorMode.GRAY then
+      return app.pixelColor.greya(0, 0)
+    elseif sprite.ColorMode == ColorMode.INDEXED then
+      return sprite.transparentColor
+    else
+      return 0
+    end
+  end
+
+  function isTileEmpty(tileX, tileY)
+    local empty = getEmptyColor()
+
+    for y = 0, sizeY - 1 do
+      for x = 0, sizeX - 1 do
+        if image:getPixel(tileX * sizeX + x, tileY * sizeY + y) ~= empty then
+          return false
+        end
+      end
+    end
+
+    return true
+  end
+
+  function isDuplicate(x1, y1, x2, y2)
+    return not (x1 == x2 and y1 == y2)
+       and not isTileEmpty(x1, y1)
+       and not isTileEmpty(x2, y2)
+       and tileEquals(x1, y1, x2, y2)
+  end
+
+  function clear(iterator)
+    for pixel in iterator do pixel(getEmptyColor()) end
+  end
+  
+  app.transaction(function()
+    for tileY = 0, tilesY - 1 do
+      for tileX = 0, tilesX - 1 do
+        for otherY = 0, tilesY - 1 do
+          for otherX = 0, tilesX - 1 do
+            if isDuplicate(tileX, tileY, otherX, otherY) then
+              clear(image:pixels(Rectangle(otherX * sizeX, otherY * sizeY, sizeX, sizeY)))
+            end
+          end
+        end
+      end
+    end
+  end)
+end
